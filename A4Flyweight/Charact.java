468c4d807442bf37341ceb9b59937b6d5bfbd44c
Jacob Gordon <jacoballengordon@gmail.com>
2020-01-27 20:00:39 -0800

    Add files via upload

---
+++ A4Flyweight/Charact.java
@@ -0,0 +1,8 @@
+
+public class Charact {
+	char unicode;
+	
+	Charact(char unicode) {
+		this.unicode = unicode;
+	}
+}
---
+++ A4Flyweight/Font.java
@@ -0,0 +1,12 @@
+
+public class Font {
+	String fontName;
+	int pointSize;
+	String style;
+	
+	Font(String font, int point, String style) {
+		this.fontName = font;
+		this.pointSize = point;
+		this.style = style;
+	}
+}
